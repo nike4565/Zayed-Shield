37150230ff3283129f48ed4b162958288bae99d6
7 <3>
2026-03-10 12:00:00 +0400

    🔧 تحديث شامل: إعدادات، سكربتات، وملفات العنبكون

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+// webpack.config.js
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
-module.exports = {
+export default {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: isDevelopment ? 'bundle.js' : 'bundle.[contenthash].js',
-    publicPath: '/',
+    path: path.resolve('dist'),
+    filename: 'bundle.js',
+    clean: true,
   },
-  mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  mode: 'development',
   devServer: {
-    static: path.join(__dirname, 'dist'),
-    historyApiFallback: true,
-    compress: true,
-    port: 3000,
+    static: './dist',
+    port: 5173,
     open: true,
     hot: true,
   },
@@ -28,70 +21,23 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/i,
-        use: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(scss|sass)$/i,
-        use: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-    },
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/favicon.ico',
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[contenthash].css',
+      title: 'Zayed Shield',
     }),
   ],
-  optimization: {
-    minimize: !isDevelopment,
-    minimizer: [
-      new TerserPlugin({
-        test: /\.js(\?.*)?$/i,
-        exclude: /\/excluded-folder/, // غيّر هذا حسب الحاجة
-        parallel: 4,
-        terserOptions: {
-          ecma: 2020,
-          compress: true,
-          mangle: true,
-          format: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-    splitChunks: {
-      chunks: 'all',
-    },
-    runtimeChunk: 'single',
-  },
 };
